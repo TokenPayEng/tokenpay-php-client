fff0501cdfb905d68ccca591a18b3944f523d6c6
Sabri Onur Tüzün <onur@craftbase.io>
2020-11-23 22:46:17 +0300

    changes parameter data

--- samples/approve_payment_transactions.php
+++ samples/approve_payment_transactions.php
@@ -4,7 +4,7 @@ require_once('config/sample_config.php');
 
 $request = array(
     'paymentTransactionIds' => array(1, 2),
-    'isTransactional' => false
+    'isTransactional' => true
 );
 
 $response = FunctionalTestConfig::tokenpay()->payment()->approvePaymentTransactions($request);
